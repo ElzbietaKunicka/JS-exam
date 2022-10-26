b02f3ffd15a73ad362276dba2e9ca0eb0c9dbd63
elzzzbieta <elzbieta.petkevic@gmail.com>
2022-10-26 23:16:38 +0300

    Add solution for task 1

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,32 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+const user = document.querySelector("form");
+
+user.addEventListener("submit", weightConverter);
+
+function weightConverter(e) {
+  e.preventDefault();
+  const userWeight = Number(document.querySelector("#search").value);
+
+  const weightToLb = Math.round(userWeight * 2.2046 * 10) / 10;
+  const weightToG = Math.round((userWeight / 0.001) * 10) / 10;
+  const weightToOz = Math.round(userWeight * 35.274 * 10) / 10;
+
+  //create outputParagraf
+
+  const paragrafWithLb = document.createElement("p");
+  paragrafWithLb.textContent = "1. Svarai (lb): " + weightToLb;
+
+  const paragrafWithGr = document.createElement("p");
+  paragrafWithGr.textContent = "2. Gramai (g): " + weightToG;
+
+  const paragrafWithOz = document.createElement("p");
+  paragrafWithOz.textContent = "3. Uncijos (oz): " + weightToOz;
+
+  document
+    .getElementById("output")
+    .append(paragrafWithLb, paragrafWithGr, paragrafWithOz);
+
+  document.querySelector("form").reset();
+};
