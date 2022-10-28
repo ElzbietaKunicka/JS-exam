40b2061bfa518b4f6d95159a3b31bce35d366a9d
elzzzbieta <elzbieta.petkevic@gmail.com>
2022-10-28 21:29:51 +0300

    Delete exports (task 10)

--- Task 10/script.js
+++ Task 10/script.js
@@ -1,20 +1,20 @@
 /* ------------------------------ TASK 10 ---------------------------------------------------
 Sutvarkykite užduoties "Task 10" esančius failus taip, kad veiktų žemiau pateiktos funkcijos.
 -------------------------------------------------------------------------------------------- */
-import {composition} from "./modules/math/composition.js";
-import {division} from "./modules/math/division.js";
-import {five} from "./modules/numbers/numbers.js";
-import {four} from "./modules/numbers/numbers.js";
-import {multiplication} from "./modules/math/multiplication.js";
-import {one} from "./modules/numbers/numbers.js";
-import {substraction} from "./modules/math/subtraction.js";
-import {three} from "./modules/numbers/numbers.js";
-import {two} from "./modules/numbers/numbers.js";
+import { composition } from "./modules/math/composition.js";
+import { division } from "./modules/math/division.js";
+import { five } from "./modules/numbers/numbers.js";
+import { four } from "./modules/numbers/numbers.js";
+import { multiplication } from "./modules/math/multiplication.js";
+import { one } from "./modules/numbers/numbers.js";
+import { substraction } from "./modules/math/subtraction.js";
+import { three } from "./modules/numbers/numbers.js";
+import { two } from "./modules/numbers/numbers.js";
 
-export let a = composition(one, four);
-export let b = division(four, two);
-export let c = substraction(three, two);
-export let d = multiplication(five, two);
+let a = composition(one, four);
+let b = division(four, two);
+let c = substraction(three, two);
+let d = multiplication(five, two);
 
 console.log(a);
 console.log(b);
