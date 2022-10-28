6fb7bfe9e39629534bd2b823e4203c5d583e0060
elzzzbieta <elzbieta.petkevic@gmail.com>
2022-10-28 21:50:41 +0300

    Change numbers import in one line task 10

--- Task 10/script.js
+++ Task 10/script.js
@@ -1,15 +1,12 @@
+import { five, four, one, three, two } from "./modules/numbers/numbers.js";
+
 /* ------------------------------ TASK 10 ---------------------------------------------------
 Sutvarkykite užduoties "Task 10" esančius failus taip, kad veiktų žemiau pateiktos funkcijos.
 -------------------------------------------------------------------------------------------- */
 import { composition } from "./modules/math/composition.js";
 import { division } from "./modules/math/division.js";
-import { five } from "./modules/numbers/numbers.js";
-import { four } from "./modules/numbers/numbers.js";
 import { multiplication } from "./modules/math/multiplication.js";
-import { one } from "./modules/numbers/numbers.js";
 import { substraction } from "./modules/math/subtraction.js";
-import { three } from "./modules/numbers/numbers.js";
-import { two } from "./modules/numbers/numbers.js";
 
 let a = composition(one, four);
 let b = division(four, two);
